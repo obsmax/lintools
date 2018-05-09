61b483d577393c3784a585235095270d903fba17
maximilien <max@maxbook.local>
2018-05-09 14:16:32 +0200

    simplify kill_

--- src/kill_.py
+++ src/kill_.py
@@ -7,12 +7,7 @@ kill_ -SIGTERM "some command visible in ps_"
 """
 
 from subprocess import Popen, PIPE
-def whoami():
-    script = "whoami"
-    p = Popen(script, stdout=PIPE, shell = True)#, stderr = stderrfid)
-    stdout, stderr = p.communicate()
-    user = stdout.strip().strip('\n').strip()
-    return user
+
 def home_path():
     script = "cd ~; pwd"
     p = Popen(script, stdout=PIPE, shell = True)#, stderr = stderrfid)
@@ -20,7 +15,6 @@ def home_path():
     h = stdout.strip().strip('\n').strip()
     return h
 home = home_path()
-user = whoami()
 
 if len(sys.argv) == 1:
     print help_message
@@ -29,20 +23,21 @@ if len(sys.argv) == 1:
 ss = sys.argv[1:]
 assert ss[0].startswith('-')
 option = ss[0]
+tmpfile = '%s/.kill_.tmp.out' % home
 
-if os.path.exists('%s/.kill_.tmp.out' % home):
-    os.remove('%s/.kill_.tmp.out' % home)
+if os.path.exists(tmpfile):
+    os.remove(tmpfile)
 
 for s in ss:
    if s == option: continue
-   os.system("""ps_ | grep "%s" | awk '{print "kill %s "$2" #"$0}' | grep -v 'grep' | grep -v 'kill_' >> %s/.kill_.tmp.out""" % (s, option, home))
+   os.system("""ps_ | grep "%s" | awk '{print "kill %s "$2" #"$0}' | grep -v 'grep' | grep -v 'kill_' >> %s""" % (s, option, tmpfile))
 
 
-os.system('cat %s/.kill_.tmp.out' % home)
+os.system('cat %s' % tmpfile)
 if raw_input('run?') == "y":
-    os.system("bash %s/.kill_.tmp.out" % home)
+    os.system("bash %s" % tmpfile)
 
-os.system('rm -f %s/.kill_.tmp.out' % home)
+os.system('rm -f %s' % tmpfile)
     
 
 
