114b6119734d54dca85b3007b7a0b8617a40251c
maximilien <max@maxbook.local>
2018-05-18 16:02:41 +0200

    add module to execute linux commands as python functions

---
+++ lin2py/linuxcmds.py
@@ -0,0 +1,24 @@
+import subprocess
+
+def execbash(script):
+    proc = subprocess.Popen("/bin/bash", stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, err = proc.communicate(script)
+    return out, err
+
+def whoami():
+    return execbash('whoami')[0].split('\n')[0].strip()
+
+def home():
+    return execbash('echo $HOME')[0].split('\n')[0].strip()
+
+def pwd():
+    return execbash('pwd')[0].split('\n')[0].strip()
+
+def ll(path):
+    return execbash('ls -lrth %s' % path)[0]
+
+if __name__ == "__main__":
+    print whoami()
+    print home()
+    print pwd()
+    print ll('./*')
