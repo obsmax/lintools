db35464c2ef26401945da5b6781e542858ec9214
obsmax <maximilien.lehujeur@gmail.com>
2018-08-17 15:56:00 +0200

    stop writing to /tmp

--- src/count.py
+++ src/count.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 import glob, os, sys
+from subprocess import Popen, PIPE
 
+def whoami():
+    script = "whoami"
+    p = Popen(script, stdout=PIPE, shell = True)#, stderr = stderrfid)
+    stdout, stderr = p.communicate()
+    user = stdout.strip().strip('\n').strip()
+    return user
+def home_path():
+    script = "cd ~; pwd"
+    p = Popen(script, stdout=PIPE, shell = True)#, stderr = stderrfid)
+    stdout, stderr = p.communicate()
+    h = stdout.strip().strip('\n').strip()
+    return h
+home = home_path()
 
 ###########################################
 def countfiles(paths):
@@ -10,10 +24,10 @@ def countfiles(paths):
         if not os.path.isdir(d):
             NN += 1
             continue
-        cmd = 'find -L %s -type f | wc -l > /tmp/countf.log' % d
+        cmd = 'find -L %s -type f | wc -l > ~/.countf.log' % d
         os.system(cmd)
         print d,
-        with open('/tmp/countf.log', 'r') as fid:
+        with open('{}/.countf.log'.format(home), 'r') as fid:
             n = int(fid.readline().split('\n')[0])
             print n
             N += n
@@ -28,10 +42,10 @@ def countdirs(paths):
     N = 0
     for d in paths:
         if not os.path.isdir(d): continue
-        cmd = 'find -L %s -type d | wc -l > /tmp/countd.log' % d
+        cmd = 'find -L %s -type d | wc -l > ~/.countd.log' % d
         os.system(cmd)
         print d,
-        with open('/tmp/countd.log', 'r') as fid:
+        with open('{}/.countd.log'.format(home), 'r') as fid:
             n = int(fid.readline().split('\n')[0])
             print n
             N += n
